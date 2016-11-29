4ac307cdce64876f09387e11412b4e343afa1235
Mario Octavio <octavio.jimenez@hpe.com>
2016-11-28 18:33:13 -0600

    added test

--- test/HomeController.spec.js
+++ test/HomeController.spec.js
@@ -1,20 +1,17 @@
-// Load the module containing the app, only 'ng' is loaded by default.
 describe('demoApp', function() {
 
     beforeEach(module('demoApp'));
-    // 
-    // var $controller;
-    //
-    // beforeEach(inject(function(_$controller_) {
-    //     // The injector unwraps the underscores (_) from around the parameter names when matching
-    // //     $controller = _$controller_;
-    // }));
+
+    var $controller;
+
+    beforeEach(inject(function(_$controller_) {
+        $controller = _$controller_;
+    }));
 
     describe('home controller', function() {
         it('should be defined', function() {
-            // const homeCtrl = $controller('HomeController');
-            // expect(homeCtrl).toBeDefined();
-            expect(true).toBe(true);
+            const homeCtrl = $controller('HomeController');
+            expect(homeCtrl).toBeDefined();
         });
     });
 });
